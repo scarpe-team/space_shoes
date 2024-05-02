33fcde7776c5edda3f56f4e41d2fb4b19de084eb
Noah Gibbs <the.codefolio.guy@gmail.com>
2024-05-02 20:34:07 +0100

    Support exe/space_shoes with -v and test it

--- test/test_space_shoes.rb
+++ test/test_space_shoes.rb
@@ -8,7 +8,30 @@ class TestSpaceShoes < Minitest::Test
   end
 end
 
-class TestSpaceShoesCommand
-  def test_with_dash_v
+class TestSpaceShoesCommand < Minitest::Test
+  ROOT_DIR = File.expand_path(File.join(__dir__, ".."))
+
+  def out_or_fail(cmd)
+    out = `#{cmd}`
+    unless $?.success?
+      raise SpaceShoes::Error, "Failed while trying to run command: #{cmd.inspect}"
+    end
+    out
+  end
+
+  def test_space_shoes_cmd_with_dash_v
+    Dir.chdir(ROOT_DIR) do
+      out = out_or_fail "exe/space_shoes --dev -v"
+      assert_includes out, "SpaceShoes"
+      assert_includes out, "Scarpe-Components"
+      assert_includes out, "Lacci"
+    end
+  end
+
+  def test_space_shoes_dash_cmd
+    Dir.chdir(ROOT_DIR) do
+      out = out_or_fail "exe/space-shoes --dev -v"
+      assert_includes out, "SpaceShoes"
+    end
   end
 end
