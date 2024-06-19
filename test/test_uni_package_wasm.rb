16ae9464cc4735627602ba0d824d0fd7822b8091
Noah Gibbs <the.codefolio.guy@gmail.com>
2024-06-19 16:18:37 +0100

    Another Capybara test for SpaceShoes

--- test/test_uni_package_wasm.rb
+++ test/test_uni_package_wasm.rb
@@ -2,6 +2,11 @@
 
 require "test_helper"
 
+# These tests start up Selenium and headless Chrome. They
+# run a local build of SpaceShoes and then check Capybara
+# assertions (based on Selenium) for what DOM elements
+# appear, and run Capybara actions to do things like press
+# buttons.
 class Scarpe::TestUnifiedPackageWasm < SpaceShoesPackagedTest
   def test_app_runs
     with_app("/app_tiny_button.html") do
@@ -9,15 +14,15 @@ class Scarpe::TestUnifiedPackageWasm < SpaceShoesPackagedTest
     end
   end
 
-#  def test_button_creates_alert
-#    with_app("button_alert") do
-#      assert_selector("button")
-#      assert_no_text("Aha!")
-#      click_button("Push me")
-#      assert_text("Aha!")
-#    end
-#  end
-#
+  def test_button_creates_alert
+    with_app("/app_tiny_button.html") do
+      assert_selector("button")
+      assert_no_text("Aha!")
+      click_button("OK")
+      assert_text("Aha!")
+    end
+  end
+
 #  def test_widgets_exist
 #    with_app("widgets_basic") do
 #      assert_selector("button", wait: 5)
