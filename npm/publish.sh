aabb58a72d7501c9b82ea6cc5101af9ef2826b67
Noah Gibbs <the.codefolio.guy@gmail.com>
2024-07-09 20:38:42 +0100

    More minor NPM work

--- npm/publish.sh
+++ npm/publish.sh
@@ -7,4 +7,4 @@ set -e
 cp ../packaging/packed_ruby.wasm .
 ~/Desktop/binaryen-version_117/bin/wasm-opt packed_ruby.wasm -Os -o packed_ruby.wasm
 cp ../test/cache/spacewalk.js .
-npm publish --access public
+#npm publish --access public
