0742656d717481935f99621c8bca1387e423fd43
Noah Gibbs <the.codefolio.guy@gmail.com>
2024-07-09 13:46:27 +0100

    Fix missed change to how boolean test-passed variable is stored

--- test/test_shoes_spec.rb
+++ test/test_shoes_spec.rb
@@ -7,7 +7,7 @@ class Scarpe::TestShoesSpecInfrastructure < SpaceShoesPackagedTest
   def test_basic_sspec_test_succeeds
     with_app("/sspec_test.html") do
       assert_selector("div.minitest_result", wait: 5)
-      result = page.evaluate_script('document.shoes_spec_passed')
+      result = page.evaluate_script('document.shoes_spec.passed')
       assert_equal true, result
     end
   end
