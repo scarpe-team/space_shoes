0319a2fe12e736200577b52b9c7cb04ed659ebf4
Noah Gibbs <the.codefolio.guy@gmail.com>
2024-07-20 15:22:34 +0100

    Add ShoesSpec failure test

--- test/test_shoes_spec.rb
+++ test/test_shoes_spec.rb
@@ -11,4 +11,14 @@ class Scarpe::TestShoesSpecInfrastructure < SpaceShoesPackagedTest
       assert_equal true, result
     end
   end
+
+  def test_basic_sspec_test_fails
+    with_app("/sspec_fail.html") do
+      assert_selector("div.minitest_result", wait: 5)
+      result = page.evaluate_script('document.shoes_spec.passed')
+      assert_equal false, result, "A test failure should return a false result for shoes_spec.passed"
+      fails = page.evaluate_script('document.shoes_spec.failures')
+      assert_equal 1, fails, "Should see one failure, not #{fails.inspect}"
+    end
+  end
 end
